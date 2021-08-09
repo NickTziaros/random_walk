1f0d206c9a34ac3af1b08ea536641ab1d9d510b4
NickTziaros <tziaros.nikos@gmail.com>
2021-08-09 14:42:14 +0300

    Von Mises Distribution

--- src/rw_node.py
+++ src/rw_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import	rospy
 import 	sys
+import  numpy as np
 from 	geometry_msgs.msg import Twist,Point,Pose
 from 	nav_msgs.msg import Odometry
 from	robot_class import robot
@@ -11,10 +12,13 @@ def main():
     
 
     while not rospy.is_shutdown():
-        r.publish_vel(0.1,0.2) 
-        odom=r.get_odom()
-        print(r.get_odom())
-        print(l.closest_point())
+        r.publish_vel(0.1,0.2)
+        # if l.closest_point() < 0.6:
+        p=np.random.vonmises(0,0.4)
+        print(p)
+        # odom=r.get_odom()
+        # print(r.get_odom())
+        # print(l.closest_point())
 
 
 if __name__ == '__main__':
