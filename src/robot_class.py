5d3754e9c598e5379ff96c0d32843f94dea041ab
NickTziaros <tziaros.nikos@gmail.com>
2021-08-12 20:53:21 +0300

    Added fix_yaw funciton to Robot Class.The programma stayes there till the yaw is fixed with the one from the random von mises heading

--- src/robot_class.py
+++ src/robot_class.py
@@ -3,9 +3,11 @@ import  rospy
 from    geometry_msgs.msg import Twist,Point,Pose
 from    nav_msgs.msg import Odometry
 from    math    import sqrt,pow,atan2,pi,cos,sin
+from 	tf import transformations
 from    tf.transformations import euler_from_quaternion
 from    random import *
 import  numpy
+import  math
 
 class robot():
 
@@ -17,6 +19,8 @@ class robot():
 		
 	def sub_callback(self,msg):
 		self.robot_pose=[msg.pose.pose.position.x,msg.pose.pose.position.y]
+		rot_q= msg.pose.pose.orientation
+		(roll,pitch,self.yaw)= euler_from_quaternion([rot_q.x,rot_q.y,rot_q.z,rot_q.w]) 
 
 
 	def publish_vel(self,linear,angular):
@@ -28,3 +32,39 @@ class robot():
 		# rospy.loginfo(self.robot_pose[0])
 		# rospy.loginfo(self.robot_pose[1])
 		return self.robot_pose
+
+	def get_yaw_deg(self):
+		return self.rad2deg(self.yaw)
+
+	def euclidean_distance(self, goal_point):
+		distance= sqrt(pow((goal_point[0] - self.robot_pose[0]), 2) +
+				pow((goal_point[1] - self.robot_pose[1]), 2))
+
+		return distance
+
+	def rad2deg(self,rad):
+		deg=round((rad*180)/pi,4)
+		return deg
+
+	def fix_yaw(self,des_yaw):
+		rospy.loginfo(self.rad2deg(des_yaw))
+		yaw_deg=self.get_yaw_deg()
+		angle_diff= self.rad2deg(des_yaw) - yaw_deg
+        # rospy.loginfo('yaw_deg   %s',yaw_deg)
+        # rospy.loginfo('angle goal%s',self.angle_deg(goal_point))
+		while abs(angle_diff )>5  and not rospy.is_shutdown():
+			if angle_diff < 0: 
+				speed=-0.2
+			else:
+				speed= 0.2
+			angle_diff= self.rad2deg(des_yaw) - self.get_yaw_deg()
+			rospy.loginfo(angle_diff)
+			self.publish_vel(0,speed)
+		
+
+
+
+
+
+# if __name__ == '__main__':
+# 	robot()
--- src/rw_node.py
+++ src/rw_node.py
@@ -6,17 +6,39 @@ from 	geometry_msgs.msg import Twist,Point,Pose
 from 	nav_msgs.msg import Odometry
 from	robot_class import robot
 from    laser_class import laser
+import  time    
 
 
 def main():
     
 
     while not rospy.is_shutdown():
-        r.publish_vel(0.1,0.2)
+        step=0.5
         # if l.closest_point() < 0.6:
-        p=np.random.vonmises(0,0.4)
-        print(p)
-        # odom=r.get_odom()
+        odom=r.get_odom()
+        distance=0
+        new_heading=np.random.vonmises(0,0.4)
+        r.fix_yaw(new_heading)
+        r.publish_vel(0.0,0)
+        time.sleep(6)
+        rospy.loginfo("lol")
+        r.publish_vel(0,0)
+        rospy.loginfo(distance)
+        if step-distance>0.05 :
+            r.publish_vel(0.1,0)
+            distance=r.euclidean_distance(odom)
+            # rospy.loginfo(distance)
+
+        # else:
+        #     new_heading=np.random.vonmises(0,0.4)
+        #     rospy.loginfo(new_heading)
+
+        #     r.publish_vel(0.0,0)
+        #     r.fix_yaw(new_heading)
+        
+
+
+
         # print(r.get_odom())
         # print(l.closest_point())
 
