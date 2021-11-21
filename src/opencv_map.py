4a25b380d4bbcb0758723d61d759fa3f57f95f29
NickTziaros <tziaros.nikos@gmail.com>
2021-11-21 11:29:48 +0200

    Added Image Metrics

--- src/opencv_map.py
+++ src/opencv_map.py
@@ -6,9 +6,9 @@ import cv2
 img = cv2.imread("/home/nikos/catkin_ws/src/random_walk/stage_results/ground_truth_1.pgm",0)
 img5=	cv2.imread("/home/nikos/catkin_ws/src/random_walk/stage_results/try_1lol.pgm",0)
 img2 = cv2.imread("/home/nikos/catkin_ws/src/random_walk/stage_results/try_1lol.pgm",0)
-img3= img[40:440,40:440]
-img6= img[40:440,40:440]
-img4=img2[40:440,40:440]
+img3= img[42:438,42:438]
+img6= img[42:438,42:438]
+img4=img2[42:438,42:438]
 cv2.imshow("croped",img3)
 cv2.imshow("croped2",img4)
 
